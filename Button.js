7f2d29ce94341285f5c7ad5e18554408ef829e8e
Clément <clement33.moreaupro@gmail.com>
2024-03-08 12:13:20 +0100

    Suppression de fichiers obsolètes

--- Button.js
+++
@@ -1,9 +0,0 @@
-let bouton = document.getElementById('BoutonCompteur');
-let affichage = document.getElementById('AffichageCompteur');
-
-let compteur = 0;
-
-bouton.addEventListener('click', function(){
-    compteur++;
-    affichage.textContent = 'Nombre de clics : ' + compteur;
-});
