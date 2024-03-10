18895c67443a28c1e33446ce41888baec13ab328
Clément <clement33.moreaupro@gmail.com>
2024-03-10 21:37:13 +0100

    Différence Const - Let

---
+++ Base_JS_06.js
@@ -0,0 +1,4 @@
+const Celsius = 31;     // "Const" une valeur qui ne bouge pas
+let Farenheit = (Celsius * 9/5) + 32;   // "Let" une valeur qui peut varier
+
+console.log("La température en Farenheit est de ", Farenheit);
