a6597d634b294e261afc33a97020811164a3a950
Clément <clement33.moreaupro@gmail.com>
2024-03-14 00:18:04 +0100

    Exercice - Calculatrice

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -17,6 +17,36 @@ for (let i = 0; i < boutonsE1.length; i++) {
     });
 }
 
+const boutonEgal = document.querySelector(".egal");
+
+boutonEgal.addEventListener("mouseover", (event) => {
+    const x = event.pageX - boutonEgal.offsetLeft;
+    const y = event.pageY - boutonEgal.offsetTop;
+
+    boutonEgal.style.setProperty("--xPos", x + "px");
+    boutonEgal.style.setProperty("--yPos", y + "px");
+});
+
+const boutonReset = document.querySelector(".reset");
+
+boutonReset.addEventListener("mouseover", (event) => {
+    const x = event.pageX - boutonReset.offsetLeft;
+    const y = event.pageY - boutonReset.offsetTop;
+
+    boutonReset.style.setProperty("--xPos", x + "px");
+    boutonReset.style.setProperty("--yPos", y + "px");
+});
+
+const boutonOperation = document.querySelectorAll(".operation");
+
+boutonOperation.addEventListener("mouseover", (event) => {
+    const x = event.pageX - boutonOperation.offsetLeft;
+    const y = event.pageY - boutonOperation.offsetTop;
+
+    boutonOperation.style.setProperty("--xPos", x + "px");
+    boutonOperation.style.setProperty("--yPos", y + "px");
+});
+
 function effaceResultat () {
     const texteAffiche = champValeur.value;
     champValeur.value = texteAffiche.slice(0, -1);
